8f1b357713d44782ee23042ef69a5ddceb9180e6
Christian Possidonio <=>
2025-04-21 19:19:18 -0300

    fix: ao criar um card o status sempre estava em BACKLOG, precisei mudar para ser tornar dinamico

--- src/application/use-cases/create-task-usecase.ts
+++ src/application/use-cases/create-task-usecase.ts
@@ -5,8 +5,8 @@ import { CreateTaskDto } from '../../interfaces/dtos/create-task.dto';
 export class CreateTaskUseCase {
   constructor(private readonly repository: TaskRepository) {}
 
-  async execute({ title }: CreateTaskDto): Promise<Task> {
-    const task = new Task(crypto.randomUUID(), title, 'BACKLOG', new Date());
+  async execute({ title, status }: CreateTaskDto): Promise<Task> {
+    const task = new Task(crypto.randomUUID(), title, status, new Date());
     await this.repository.create(task);
     return task;
   }
--- src/interfaces/dtos/create-task.dto.ts
+++ src/interfaces/dtos/create-task.dto.ts
@@ -3,4 +3,7 @@ import { ApiProperty } from '@nestjs/swagger';
 export class CreateTaskDto {
   @ApiProperty({ example: 'New Task created!' })
   title: string;
+
+  @ApiProperty({ example: 'BACKLOG', enum: ['BACKLOG', 'DOING', 'DONE'] })
+  status: 'BACKLOG' | 'DOING' | 'DONE';
 }
